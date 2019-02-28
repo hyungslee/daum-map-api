86d978d03075947570b5d9ab3abf3c6a0f7a74d0
corine <filterlee@gmail.com>
2019-02-28 15:20:14 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import DaumMapComponent from "./component/DaumMapComponent";
+import Marker from "./component/tools/Marker";
+import Polygon from "./component/tools/Polygon";
+import MapControlPanel from "./component/tools/MapControlPanel";
+import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <h1>App</h1>
+        <DaumMapComponent position={{ lat: 37.54841, lng: 126.98068 }} zoom="6">
+          <Marker position={{ lat: 37.54841, lng: 126.98068 }} />
+          <Marker position={{ lat: 37.55469, lng: 126.97075 }} />
+          <Polygon
+            path={[
+              { lat: 37.54601, lng: 126.97843 },
+              { lat: 37.55319, lng: 126.97637 },
+              { lat: 37.55428, lng: 126.97253 },
+              { lat: 37.54936, lng: 126.97158 },
+              { lat: 37.54507, lng: 126.97186 },
+              { lat: 37.54043, lng: 126.97319 },
+              { lat: 37.54063, lng: 126.97467 },
+              { lat: 37.54527, lng: 126.97356 }
+            ]}
+          />
+        </DaumMapComponent>
+        <MapControlPanel />
+      </React.Fragment>
     );
   }
 }
-
-export default App;
